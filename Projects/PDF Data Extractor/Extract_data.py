b70d566cd39b409ab3b231c92030993efda801a0
Sneha Kothi <sneha.kothi@somaiya.edu>
2021-10-30 17:30:23 +0530

    Revert "Pdf data extractor"
    This reverts commit 7244a09c6f738bee3bfaed557b98bcae89c13df8.

--- Projects/PDF Data Extractor/Extract_data.py
+++
@@ -1,7 +0,0 @@
-from pdf_data_extraction import pdf_data
-
-# provide path of the required pdf 
-# pdf_data.pdf_img('D:\SNEHA\DataScience_Materials\Projects\PDF Data Extractor\invoice (5).pdf')
-
-#provide path of the converted image
-pdf_data.create_file('D:\SNEHA\DataScience_Materials\Projects\PDF Data Extractor\page0.jpg')
